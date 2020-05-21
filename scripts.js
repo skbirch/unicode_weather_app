74186e343689fd270f5b88be35014c0a6dd68048
Samuel Birch <samkbirch@gmail.com>
2020-05-20 21:33:55 -0500

    Update scripts.js
    added console log

--- scripts.js
+++ scripts.js
@@ -11,7 +11,16 @@ var icons = [["Rain", "&#127783"],
 ["Snow", "&#127784"],
 ["Tornado", "&#127786"],
 ["Clear", "&#9728"],
-["Clouds", "&#127780"]]
+["Clouds", "&#9729"],
+["Mist", "&#9729"],
+["Smoke", "&#9729"],
+["Haze", "&#9729"],
+["Dust", "&#9729"],
+["Fog", "&#9729"],
+["Sand", "&#9729"],
+["Ash", "&#9729"],
+["Squall", "&#127785"]
+]
 
 function changeTitle(city) {
     document.getElementById("title").innerHTML = "Weather in: "+ city;
@@ -25,7 +34,19 @@ function changeUnicode(name) {
 }
 
 
+//log IP
+fetch("https://api.ipify.org/?format=json").then(response => response.json()).then(data => console.log(data.ip))
+
+//log City
+
+fetch("https://api.ipify.org/?format=json").then(response => response.json()).then(data => fetch("http://api.ipstack.com/"+data.ip+"?access_key="+ ipkey).then(response => response.json()).then(data => console.log(data.city)))
+
+//log weather
+fetch("https://api.ipify.org/?format=json").then(response => response.json()).then(data => fetch("http://api.ipstack.com/"+data.ip+"?access_key="+ ipkey).then(response => response.json()).then(data => fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ data.latitude +"&lon="+ data.longitude +"&appid="+weatherkey).then(response => response.json()).then(data => console.log(data.current.weather[0].main))))
+
+//set unicode to weather
 fetch("https://api.ipify.org/?format=json").then(response => response.json()).then(data => fetch("http://api.ipstack.com/"+data.ip+"?access_key="+ ipkey).then(response => response.json()).then(data => fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ data.latitude +"&lon="+ data.longitude +"&appid="+weatherkey).then(response => response.json()).then(data => changeUnicode(data.current.weather[0].main))))
 
+//set city
 fetch("https://api.ipify.org/?format=json").then(response => response.json()).then(data => fetch("http://api.ipstack.com/"+data.ip+"?access_key="+ ipkey).then(response => response.json()).then(data => changeTitle(data.city)))
 
